ad39e17b54dc1cb72517e68dd631ef7078b4f238
gitfof <137779427+gitfof@users.noreply.github.com>
2023-08-11 19:32:02 +0200

    Update my_4dof_subscriber.py

--- my_4dof_robot/my_4dof_subscriber.py
+++ my_4dof_robot/my_4dof_subscriber.py
@@ -18,14 +18,23 @@ class Robot_Subscriber(Node):
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.linear.x)
         with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-            time.sleep(0.1);
+            time.sleep(2);
             if arduino.isOpen():
                 try:
-                    while True:
-                        if msg.linear.x>0:
-                            arduino.write("bal");
-                        elif msg.linear.x<0:
-                            arduino.write("jobb");
+                    if msg.linear.x > 0:
+                        bla = "bal"
+                        if arduino.in_waiting==0:
+                            for i in bla:
+                                arduino.write(bytes(i, "UTF-8"))
+                                time.sleep(0.2)
+                            print("balra megyunk")
+                    elif msg.linear.x<0:
+                        bla = "jobb"
+                        if arduino.in_waiting==0:
+                            for i in bla:
+                                arduino.write(bytes(i, "UTF-8"))
+                                time.sleep(0.2)
+                            print("jobbra megyunk")    
                 except:
                     print("hiba");  
 
