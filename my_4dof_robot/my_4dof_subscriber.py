15ace7249241817afcce69f118a7a87306672548
gitfof <137779427+gitfof@users.noreply.github.com>
2023-08-19 13:41:10 +0200

    mod_v0.5

--- my_4dof_robot/my_4dof_subscriber.py
+++ my_4dof_robot/my_4dof_subscriber.py
@@ -14,84 +14,86 @@ class Robot_Subscriber(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        self.arduino = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=1)
+        time.sleep(2)
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.linear.x)
-        with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-            time.sleep(2);
-            if arduino.isOpen():
-                try:
-                    if arduino.in_waiting > 0:
-                        status = arduino.readline()
-                        status.strip()
-                        print(status)
+        #with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+            #time.sleep(2);
+        if arduino.isOpen():
+            try:
+                if arduino.in_waiting > 0:
+                    status = arduino.readline()
+                    status.strip()
+                    print(status)
                     #  Először lekérem a servo motorok státuszát az Arduinotól serial buson...
-                    bla="00"
-                    if arduino.in_waiting==0:
-                            for i in bla:
-                                arduino.write(bytes(i, "UTF-8"))
-                                time.sleep(0.2)
-                            print("státuszt kérek")
-                    while arduino.in_waiting==0: pass
+                bla="00"
+                if arduino.in_waiting==0:
+                    for i in bla:
+                        arduino.write(bytes(i, "UTF-8"))
+                        time.sleep(0.2)
+                        print("státuszt kérek")
+                while arduino.in_waiting==0: pass
                     # státusz válasz feldolgozása - servo motor aktuális szögek változóba elrakva
-                    if arduino.in_waiting > 0:
-                        status = arduino.readline()
-                        status.strip()
-                        print(status)
-                        servo_1 = int(status[3:6])     # alap motor (forgás)
-                        servo_2 = int(status[9:12])    # Joint1 (előre-hátra)
-                        servo_3 = int(status[15:18])   # Joint2 (fel-le)
-                        servo_4 = int(status[22:])     # Megfogó
+                if arduino.in_waiting > 0:
+                    status = arduino.readline()
+                    status.strip()
+                    print(status)
+                    servo_1 = int(status[3:6])     # alap motor (forgás)
+                    servo_2 = int(status[9:12])    # Joint1 (előre-hátra)
+                    servo_3 = int(status[15:18])   # Joint2 (fel-le)
+                    servo_4 = int(status[22:])     # Megfogó
                         # ezután feldolgozom a Twist üzenetben kapott mozgásinfót...
-                        if msg.angular.z != 0:
+                    if msg.angular.z != 0:
                             #servo_1 += int(msg.linear.z * 5)
-                            servo_1 = 45
-                        if msg.linear.x != 0:
+                        servo_1 = 45
+                    if msg.linear.x != 0:
                             #servo_2 += int(msg.linear.x * 10)
-                            servo_2 = 120
-                        if msg.linear.y != 0:
+                        servo_2 = 120
+                    if msg.linear.y != 0:
                             # servo_3 += int(msg.linear.y * 10)
-                            servo_3 = 130
+                        servo_3 = 130
                         # kell még a megfogó - melyik gomb legyen?????
-                        Servo_4= 25
+                    Servo_4= 25
 
                         # összerakom az üzenetet
-                        szog_1 = str(int(servo_1))
-                        if len(szog_1) == 1: 
-                            szog_1 = "00" + szog_1
-                        elif len(szog_1) == 2:
-                            szog_1 = "0" + szog_1
-                        szog_2 = str(int(servo_2))
-                        if len(szog_2) == 1: 
-                            szog_2 = "00" + szog_2
-                        elif len(szog_2) == 2:
-                            szog_2 = "0" + szog_2
-                        szog_3 = str(int(servo_3))
-                        if len(szog_3) == 1: 
-                            szog_3 = "00" + szog_3
-                        elif len(szog_3) == 2:
-                            szog_3 = "0" + szog_3
-                        szog_4 = str(int(servo_4))
-                        if len(szog_4) == 1: 
-                            szog_4 = "00" + szog_4
-                        elif len(szog_4) == 2:
-                            szog_4 = "0" + szog_4
+                    szog_1 = str(int(servo_1))
+                    if len(szog_1) == 1: 
+                        szog_1 = "00" + szog_1
+                    elif len(szog_1) == 2:
+                        szog_1 = "0" + szog_1
+                    szog_2 = str(int(servo_2))
+                    if len(szog_2) == 1: 
+                        szog_2 = "00" + szog_2
+                    elif len(szog_2) == 2:
+                        szog_2 = "0" + szog_2
+                    szog_3 = str(int(servo_3))
+                    if len(szog_3) == 1: 
+                        szog_3 = "00" + szog_3
+                    elif len(szog_3) == 2:
+                        szog_3 = "0" + szog_3
+                    szog_4 = str(int(servo_4))
+                    if len(szog_4) == 1: 
+                        szog_4 = "00" + szog_4
+                    elif len(szog_4) == 2:
+                        szog_4 = "0" + szog_4
 
-                        bla= "S1P" + szog_1 + "S2P" + szog_2 + "S3P" + szog_3 + "S4P" + szog_4
-                        print(bla)
-                        if arduino.in_waiting==0:
-                            for i in bla:
-                                arduino.write(bytes(i, "UTF-8"))
-                                time.sleep(0.2)
-                            print("mozgás!")
-                        while arduino.in_waiting==0: pass
+                    bla= "S1P" + szog_1 + "S2P" + szog_2 + "S3P" + szog_3 + "S4P" + szog_4
+                    print(bla)
+                    if arduino.in_waiting==0:
+                        for i in bla:
+                            arduino.write(bytes(i, "UTF-8"))
+                            time.sleep(0.2)
+                        print("mozgás!")
+                    while arduino.in_waiting==0: pass
                         # ellenőrzöm a futtatás eredményét
-                        if arduino.in_waiting > 0:
-                            status = arduino.readline()
-                            status.strip()
-                            print(status)
-                except:
-                    print("hiba");  
+                    if arduino.in_waiting > 0:
+                        status = arduino.readline()
+                        status.strip()
+                        print(status)
+            except:
+                print("hiba");  
 
 def main(args=None):
     rclpy.init(args=args)
